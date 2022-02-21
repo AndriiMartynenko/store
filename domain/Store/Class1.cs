8b773ae02c8e51c28036ba423d5a4b9ccb2b9935
Andrii <androosha1993@gmail.com>
2022-02-22 00:41:08 +0100

    Describe the essence of Book

---
+++ domain/Store/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Store;
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; }
+    public Book(int id, string title)
+    {
+        Title = title;
+    }
+}
